bd75c8519d0a8f441e35c3f0584f52af4676a1b8
smaiseic <siarhei.maiseichyk@gmail.com>
2024-02-29 21:46:53 +0100

    New module added

---
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GroupCreationTests {
+    private static WebDriver driver; //static в данном случае не дает переменной стать связанной с конкретным объектом и делает ее глобальной
+
+    // @BeforeAll //метод, помеченный данной аннотацией будет выполнятся перед всеми тестами в классе
+    @BeforeEach
+    public void setUp() {
+        if (driver == null) {
+            driver = new FirefoxDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("http://localhost/addressbook/index.php");
+            driver.manage().window().setSize(new Dimension(1052, 1012));
+            driver.findElement(By.name("user")).sendKeys("admin");
+            driver.findElement(By.name("pass")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
+    }
+
+//    // @AfterAll //метод, помеченный данной аннотацией будет выполнятся после всех тестов в классе
+//    @AfterEach
+//    public void tearDown() {
+//        driver.findElement(By.linkText("Logout")).click();
+//        driver.quit();
+//    }
+
+    @Test
+    public void canCreateGroup() {
+
+        driver.findElement(By.linkText("groups")).click();
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys("group name1");
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys("group header1");
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys("group footer1");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("groups")).click();
+    }
+
+    @Test
+    public void canCreateGroupWithEmptyName() {
+        driver.findElement(By.linkText("groups")).click();
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys("");
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys("");
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys("");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("groups")).click();
+    }
+}
