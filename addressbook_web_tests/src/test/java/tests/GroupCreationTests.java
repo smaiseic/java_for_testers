b8e225679d953ce2aa80125c1088be8ab89f4642
smaiseic <siarhei.maiseichyk@gmail.com>
2024-03-09 18:47:30 +0100

    Remove canCreateGroup test

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,23 +12,23 @@ import java.util.List;
 public class GroupCreationTests extends TestBase {
 
     public static List<String> groupNameProvider() {
-        var result = new ArrayList<String>();
+        var result = new ArrayList<>(List.of("group name", "group name'"));
         for (int i = 0; i < 5; i++) {
             result.add(randomString(i * 10));
         }
         return result;
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"group name", "group name'"})
-    public void canCreateGroup(String name) {
-        int groupCount = app.groups().getCount();
-        System.out.println(groupCount);
-        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
-        int newGroupCount = app.groups().getCount();
-        System.out.println(newGroupCount);
-        Assertions.assertEquals(groupCount + 1, newGroupCount);
-    }
+//    @ParameterizedTest
+//    @ValueSource(strings = {"group name", "group name'"})
+//    public void canCreateGroup(String name) {
+//        int groupCount = app.groups().getCount();
+//        System.out.println(groupCount);
+//        app.groups().createGroup(new GroupData(name, "group header", "group footer"));
+//        int newGroupCount = app.groups().getCount();
+//        System.out.println(newGroupCount);
+//        Assertions.assertEquals(groupCount + 1, newGroupCount);
+//    }
 
     @Test
     public void canCreateGroupWithEmptyName() {
