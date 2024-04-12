6bbedaf913075840d262e704baa5a58458d1a19a
smaiseic <siarhei.maiseichyk@gmail.com>
2024-04-12 20:25:03 +0200

    consumer added

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -1,20 +1,50 @@
 package ru.stqa.geometry;
 
-import ru.stqa.geometry.figures.Rectangle;
 import ru.stqa.geometry.figures.Square;
-import ru.stqa.geometry.figures.Triangle;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Geometry {
     public static void main(String[] args) {
-        Square.printSquareArea(new Square(8.0));
-        Square.printSquareArea(new Square(5.0));
+        var squares = List.of(new Square(7.0), new Square(5.0), new Square(3.0));
+//        for (Square square : squares) {
+//            Square.printSquareArea(square);
+//        }
 
-        Rectangle.printRectangleArea(3.0, 5.0);
+        // Выделяют 3 типа функций:
+        // Сonsumer (принимает на вход параметр, но ничего не возвращает)
+        // Producer (ничего на вход не принимает, но что-то возвращаетж; как-то генерирует данные)
+        // Function (что-то принимает и что-то возвращает, т.е. у нее есть и вход и выход;
+        //      трансформирует данные, которые в нее передают)
 
-        Triangle.printTriangleArea(new Triangle(2.0, 4.0, 5.0));
-        Triangle.printTriangleArea(new Triangle(4.0, 2.0, 9.0));
+//        Consumer<Square> print = (square) -> {
+//            Square.printSquareArea(square);
+//        };
 
-        Triangle.printTrianglePerimeter(new Triangle(4.0, 2.0, 5.0));
+        // (параметры) но можно скобки не ставить, если параметр один
+//        Consumer<Square> print = (square) -> {
+//            Square.printSquareArea(square);
+//        };
+
+//        Consumer<Square> print = square -> {
+//            Square.printSquareArea(square);
+//        };
+
+
+        Consumer<Square> print = (square) -> {
+            Square.printSquareArea(square);
+        };
+        squares.forEach(print);
+
+        squares.forEach(Square::printSquareArea);;
+
+//        Rectangle.printRectangleArea(3.0, 5.0);
+//
+//        Triangle.printTriangleArea(new Triangle(2.0, 4.0, 5.0));
+//        Triangle.printTriangleArea(new Triangle(4.0, 2.0, 9.0));
+//
+//        Triangle.printTrianglePerimeter(new Triangle(4.0, 2.0, 5.0));
     }
 
 }
